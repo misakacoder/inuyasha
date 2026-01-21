7b156dc3fefa63a4797bf8dd5de7691f736a2985
misakacoder <210086826@qq.com>
2026-01-21 11:21:15 +0800

    init

--- db/db.go
+++ db/db.go
@@ -18,19 +18,21 @@ func Connect(dialector func(dsn string) gorm.Dialector) {
 	once.Do(func() {
 		conf := configs.Config.Db
 		GORM = orm.New(dialector, conf)
-		GORM.Logger = func(level string, caller string, format string, args ...any) {
-			lvl, ok := logger.Parse(level)
-			if !ok {
-				lvl = logger.DEBUG
-			}
-			switch log := logger.GetLogger().(type) {
-			case *innerLogger.DynamicLevelLogger:
-				log.Push(lvl, caller, format, args...)
-			case *logger.SimpleLogger:
-				log.Push(lvl, caller, format, args...)
-			default:
-				logger.Warn("unknown logger: %T", log)
-			}
-		}
+		GORM.Logger = Logger
 	})
+}
+
+func Logger(level string, caller string, format string, args ...any) {
+	lvl, ok := logger.Parse(level)
+	if !ok {
+		lvl = logger.DEBUG
+	}
+	switch log := logger.GetLogger().(type) {
+	case *innerLogger.DynamicLevelLogger:
+		log.Push(lvl, caller, format, args...)
+	case *logger.SimpleLogger:
+		log.Push(lvl, caller, format, args...)
+	default:
+		logger.Warn("unknown logger: %T", log)
+	}
 }
